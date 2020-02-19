79970f14f54d22b38beb3066ab1f3941fdb040d8
Jussi Nousiainen <juno.athome@gmail.com>
2020-02-19 15:51:57 +0100

    Tested frouting and templates for the dynamic data rendering

--- app.py
+++ app.py
@@ -3,10 +3,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)      
 
+data = [
+    {'class_name': 'Monday Groove',
+     'class_description': 'Let it loose and sweat it out',
+     'main_element': 'Shakey shake moves',
+     'log_date': 'March 7, 2020'},
+    {'class_name': 'Tuesday Tango',
+     'class_description': 'Get down and dirty',
+     'main_element': 'Roses and shiny shoes',
+     'log_date': 'March 8, 2020'}
+]
+
+
 @app.route('/')
 @app.route('/home')           
 def home():             
-    return render_template('home.html')
+    return render_template('home.html', classes=data)
 
 @app.route('/about')           
 def about():             
