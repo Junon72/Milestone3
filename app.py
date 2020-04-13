736e788b17b8a350a14daa3c6958e9c475fb72aa
Jussi Nousiainen <juno.athome@gmail.com>
2020-04-13 18:42:23 +0200

    Error handling

--- app.py
+++ app.py
@@ -751,11 +751,13 @@ def delete_series(series_doc, series_id):
 
 @app.errorhandler(404)
 def not_found(error):
+    print('error' + error)
 	return render_template("404.html", error=error)
 
 
 @app.errorhandler(500)
 def internal_error(error):
+    print('error' + error)
 	return render_template("500.html", error=error)
 
 
